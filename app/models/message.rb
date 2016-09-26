dba4bf788a534d2d887c406a15efd327cc41e9a2
Bang Nguyen and Olivia Hinton <bang+olivia@>
2016-09-26 11:31:01 -0700

    use rest-client gem to send messages to phone

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,20 @@
 class Message < ActiveRecord::Base
-  validates :from, :to, :body, :status, :presence => true
+  before_create :send_sms
+  validates :from, :to, :body, :presence => true
+
+  private
+
+    def send_sms
+      response = RestClient::Request.new(
+      :method => :post,
+      :url => 'https://api.twilio.com/2010-04-01/Accounts/AC39a7a985f195db512a88b93ca2884057/Messages.json',
+      :user => 'AC39a7a985f195db512a88b93ca2884057',
+      :password => 'c007b6123d962dd539bcbdab8790ef3b',
+      :payload => { :Body => body,
+                    :From => from,
+                    :To => to}
+      ).execute
+    end
+
+
 end
