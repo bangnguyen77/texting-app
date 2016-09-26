581699eb66fa472e6a790b769ee5bf068f96444b
Bang Nguyen and Olivia Hinton <bang@>
2016-09-26 14:29:06 -0700

    add a form to add contacts to the address book

---
+++ db/migrate/20160926211634_add_contact_id_to_messages.rb
@@ -0,0 +1,5 @@
+class AddContactIdToMessages < ActiveRecord::Migration[5.0]
+  def change
+    add_column :messages, :contact_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160926171520) do
+ActiveRecord::Schema.define(version: 20160926211634) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.string "phone"
+  end
+
   create_table "messages", force: :cascade do |t|
-    t.string "to"
-    t.string "from"
-    t.string "body"
-    t.string "status"
+    t.string  "to"
+    t.string  "from"
+    t.string  "body"
+    t.string  "status"
+    t.integer "contact_id"
   end
 
 end
