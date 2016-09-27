94a8d2ed7c737d8e2eae38252e65080112b52369
Bang Nguyen and Olivia Hinton <olivia@>
2016-09-27 13:51:34 -0700

    add delete feature to messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -40,6 +40,12 @@ class MessagesController < ApplicationController
     @message = Message.find(params[:id])
   end
 
+  def destroy
+    @message = Message.find(params[:id])
+    @message.destroy
+    redirect_to messages_path
+  end
+
   private
   def message_params
     params.require(:message).permit(:from, :body, to:[])
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :messages, :only => [:index, :new, :create, :show] do
+  resources :messages, :only => [:index, :new, :create, :show, :destroy] do
     collection do
       post 'reply'
     end
